7df14e156308974adba7489838af9ac53e6845f3
tsmpatagonia <90854742+tsmpatagonia@users.noreply.github.com>
2021-09-17 07:40:07 -0300

    Add files via upload

--- tracker.py
+++ tracker.py
@@ -1,13 +1,14 @@
 import cartopy
 import cartopy.crs as ccrs
 import numpy as np
-import pylab as plt
+#import pylab as plt
 import ephem
 import datetime
-import os
+import requests
+import matplotlib.pyplot as plt
+
 from ephem import degree
 from datetime import datetime as date_t
-from urllib.request import urlopen
 
 def loadTLE(filename):
     """ Loads a TLE file and creates a list of satellites."""
@@ -19,7 +20,7 @@ def loadTLE(filename):
         l3 = f.readline()
         sat = ephem.readtle(l1,l2,l3)
         satlist.append(sat)
-        print (sat.name)
+        #print (sat.name)
         l1 = f.readline()
 
     f.close()
@@ -28,13 +29,26 @@ def loadTLE(filename):
 
 def main():
 	# Define observer coordinates (Wachusett Mtn.)
-	loc_lat = 42.503
-	loc_long = -71.886
+	#loc_lat = -42.503
+	#loc_long = -71.886
+	#print "Latitud"
+	loc_lat = float(input("Latitud: "))
+	#print "Longitud"
+	loc_long = float(input("Longitud: "))
+
+	fecha_str = input("Fecha y hora [dd-mm-yyyy hh:mm]: ")
+	fecha = date_t.strptime(fecha_str, "%d-%m-%Y %H:%M")
+
 	observer = ephem.Observer()
 	observer.lat = np.deg2rad(loc_lat)
 	observer.long = np.deg2rad(loc_long)
 	observer.elevation = 611 # in meters
-	observer.date = date_t.now()
+	#observer.date = date_t.now()
+	observer.date = fecha
+
+	url = 'http://celestrak.com/NORAD/elements/globalstar.txt'
+	r = requests.get(url, allow_redirects=True)
+	open('globalstar.txt', 'wb').write(r.content)
 
 	''' TODO: Check HTTP headers to pull a more up updated version 
 	file = urlopen('http://celestrak.com/NORAD/elements/globalstar.txt', timeout = 30)
@@ -49,7 +63,8 @@ def main():
 	globalstar = loadTLE("globalstar.txt")
 
 	# Define time interval (now until next 10 minutes)
-	dt = [date_t.now() + datetime.timedelta(minutes = x) for x in range(0, 10)]
+	#dt = [date_t.now() + datetime.timedelta(minutes = x) for x in range(0, 20)]
+	dt = [fecha + datetime.timedelta(minutes = x) for x in range(0, 20)]
 	sat_alt, sat_az = [], []
 	sat_lat, sat_long = [], []
 
@@ -70,14 +85,28 @@ def main():
 			sat_alt[i].append(np.rad2deg(satellite.alt))
 			sat_az[i].append(np.rad2deg(satellite.az))
 
-	plt.suptitle("Wachusett Mtn: {}".format(date_t.now()), size=18)
-	ax = plt.subplot(121, projection = ccrs.PlateCarree())
+	
+	#ax = plt.subplot(121, projection = ccrs.PlateCarree())
+	fig = plt.figure(figsize=(20,5))
+	fig.suptitle("TSMP - Globalstar SAT position : {}".format(fecha), size=10)
+	ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
+	#ax = plt.axes(projection=ccrs.PlateCarree())
+	
 	ax.add_feature(cartopy.feature.OCEAN)
 	ax.add_feature(cartopy.feature.LAND)
 	ax.add_feature(cartopy.feature.COASTLINE)
+	ax.add_feature(cartopy.feature.LAKES)
+	ax.add_feature(cartopy.feature.RIVERS)
 	ax.add_feature(cartopy.feature.BORDERS, linestyle = '-')
+	ax.gridlines()
 
-	ax.set_extent([-135, -20, -10, 80], crs=ccrs.PlateCarree())
+	mlats = loc_lat + 5
+	mlati = loc_lat - 5
+	mlongs = loc_long + 10
+	mlongi = loc_long - 10
+
+	#AMERICA ax.set_extent([  -169.99, -26.26,-53.06, 66.53 ], crs = ccrs.PlateCarree())
+	ax.set_extent([mlongi, mlongs, mlati, mlats], crs = ccrs.PlateCarree())
 
 	# Plot our reference location
 	plt.plot(loc_long, loc_lat,
@@ -105,27 +134,27 @@ def main():
 			marker = 'x',
 			transform = ccrs.Geodetic()
 		)
-	
-	# Plot satellites on polar az/el graph
-	ax = plt.subplot(122, polar=True)
-	for i in range(len(globalstar)):
-		ax.scatter(np.deg2rad(sat_az[i][0]), 
-				   90 - np.array(sat_alt[i][0]),
-				   linewidth = '3')
-		ax.annotate(globalstar[i].name[11:15], (np.deg2rad(sat_az[i][0]),90 - np.array(sat_alt[i][0])))
-		ax.plot(np.deg2rad(sat_az[i]), 90 - np.array(sat_alt[i]))
-		ax.plot(np.deg2rad(sat_az[i][-1]), 
-				90 - np.array(sat_alt[i][-1]),
-				linewidth = '2',
-				color = 'black',
-				marker = 'x'
-				)
 
-	ax.set_ylim(0,90)
-	ax.set_theta_direction(-1)
-	ax.set_theta_offset(np.pi / 2)
+	# Plot satellites on polar az/el graph
+#	ax = plt.subplot(122, polar=True)
+#	for i in range(len(globalstar)):
+#		ax.scatter(np.deg2rad(sat_az[i][0]), 
+#				   90 - np.array(sat_alt[i][0]),
+#				   linewidth = '3')
+#		ax.annotate(globalstar[i].name[11:15], (np.deg2rad(sat_az[i][0]),90 - np.array(sat_alt[i][0])))
+#		ax.plot(np.deg2rad(sat_az[i]), 90 - np.array(sat_alt[i]))
+#		ax.plot(np.deg2rad(sat_az[i][-1]), 
+#				90 - np.array(sat_alt[i][-1]),
+#				linewidth = '2',
+#				color = 'black',
+#				marker = 'x'
+#				)
+
+#	ax.set_ylim(0,90)
+#	ax.set_theta_direction(-1)
+#	ax.set_theta_offset(np.pi / 2)
 	#ax.yaxis.set_ticklabels([])
-	ax.grid(True)
+#	ax.grid(True)
 
 	plt.show()
 	
